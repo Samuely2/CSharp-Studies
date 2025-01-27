41a93ba40d0b1adb04480800bdfabb0ef19e1d8d
Samuel Vinicius <samuelviniciusoficial@gmail.com>
2025-01-26 23:10:43 -0300

    Método de extensao

---
+++ 14MetodosDeExtensao/Program.cs
@@ -0,0 +1,9 @@
+﻿using _14MetodosDeExtensao;
+
+string texto = "Usando métodos de extensão";
+
+string textoInvertido = texto.InverteString();
+
+Console.WriteLine(textoInvertido);
+
+Console.ReadKey();
