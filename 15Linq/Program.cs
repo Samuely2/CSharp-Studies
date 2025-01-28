ea3ab35290c2645be43cd3bcd130019908838ca6
Samuel Vinicius <samuelviniciusoficial@gmail.com>
2025-01-27 21:47:10 -0300

    linq

---
+++ 15Linq/Program.cs
@@ -0,0 +1,25 @@
+﻿
+List<string> nomes = new List<string>()
+{
+    "Ana", "Maria", "Pedro", "Lair", "Hugo"
+};
+//Usando a query syntax da consulta Linq
+var resultado = from m in nomes
+                where m.Contains('o')
+                select m;
+ExibirResultado(resultado);
+
+//Usando Method Syntax da consulta Linq
+var resultado1 = nomes.Where(n => n.Contains('o'));
+
+ExibirResultado(resultado1);
+Console.ReadKey();
+
+static void ExibirResultado(IEnumerable<string> resultado)
+{
+    Console.WriteLine();
+    foreach (var item in resultado)
+    {
+        Console.WriteLine(item);
+    }
+}
