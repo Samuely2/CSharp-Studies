abb7b8d60a465ba70a4d6c58f7aa77154f320cdd
Samuel Vinicius <samuelviniciusoficial@gmail.com>
2025-01-31 21:18:12 -0300

    group by e order by

---
+++ 18LinqAgrupamento/Program.cs
@@ -0,0 +1,31 @@
+﻿using _16LinqII;
+
+var listaProdutos = Produto.GetProdutos();
+
+var produtosPorCategoria = listaProdutos
+                           .GroupBy(p => p.Categoria)// agrupando por categoria
+                           .OrderBy(c => c.Key)// ordenando pela a chave
+                           .Select(g => new // criando um tipo anonimo onde categoria
+                           {
+                               Categoria = g.Key,
+                               Produtos = g.OrderBy(p => p.Nome)
+                               .Select(p => new // definindo os dados no select
+                               {
+                                   Nome = p.Nome,
+                                   Preco = p.Preco,
+                                   Estoque = p.Estoque
+                               })
+                           });
+            
+
+foreach (var grupo in produtosPorCategoria)
+{
+    Console.WriteLine(grupo.Categoria);
+
+    foreach (var produto in grupo.Produtos)
+    {
+        Console.WriteLine($" {produto.Nome} \t {produto.Preco:C2} \t{produto.Estoque}");
+    }
+}
+
+Console.ReadKey();
