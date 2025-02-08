863e9793fe1e7ab4f9ad3617ebb7c8ec854bc68f
Samuel Vinicius <samuelviniciusoficial@gmail.com>
2025-02-07 22:48:32 -0300

    ex4

---
+++ exercicio04-delegateAction/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Action<int> par = numero =>
+{
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine("O número é par");
+    }
+    else
+    {
+        Console.WriteLine("O número é impar");
+    }
+};
+
+par(5);
+
