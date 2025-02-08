82a48061b8c00030a3a7b9c29efa327c08072b6b
Samuel Vinicius <samuelviniciusoficial@gmail.com>
2025-02-07 22:05:52 -0300

    ex3

--- exercicio03-delegate/Program.cs
+++ exercicio03-delegate/Program.cs
@@ -1,3 +1,17 @@
-﻿
+﻿MensagemBoasVindas DelegateMulticast = new MensagemBoasVindas(MensagemPortugues);
 
+DelegateMulticast += MensagemIngles;
+
+DelegateMulticast.Invoke();
 Console.ReadKey();
+
+static void MensagemPortugues()
+{
+    Console.WriteLine("Bem vindos");
+}
+static void MensagemIngles()
+{
+    Console.WriteLine("Welcome");
+}
+
+public delegate void MensagemBoasVindas();
