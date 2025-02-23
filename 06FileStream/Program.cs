6748bc52013e2892328dfb93cfe0c062a2fa7dc2
Samuel Vinicius <samuelviniciusoficial@gmail.com>
2025-02-23 20:33:39 -0300

    FileStream, StreamReader

---
+++ 06FileStream/Program.cs
@@ -0,0 +1,32 @@
+﻿string caminhoArquivo = "c:\\dados\\poesia.txt";
+
+//FileStream fs = null;
+StreamReader leitor = null;
+
+try
+{
+	//fs = new FileStream(caminhoArquivo, FileMode.Open, FileAccess.Read);
+	leitor = File.OpenText(caminhoArquivo);//new StreamReader(fs);
+
+	string? linha;
+	while((linha = leitor.ReadLine()) != null)
+	{
+        Console.WriteLine(linha);
+    }
+}
+catch(IOException ex)
+{
+	Console.WriteLine(ex.Message);
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
+finally
+{
+	if (leitor != null) leitor.Close();
+	if (fs != null) fs.Close();
+}
+
+
+Console.ReadKey();
