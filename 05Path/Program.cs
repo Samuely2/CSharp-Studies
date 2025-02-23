ee7111f969ff8918fac31cf784e17f2f8bbebf3b
Samuel Vinicius <samuelviniciusoficial@gmail.com>
2025-02-23 19:35:05 -0300

    directory1

---
+++ 05Path/Program.cs
@@ -0,0 +1,15 @@
+﻿
+string caminho = "d:\\dados\\txt\\poesia.txt";
+
+char dirSeparador = Path.DirectorySeparatorChar;
+Console.WriteLine("O separador de diretório padrão é: " + dirSeparador);
+
+string[] diretorios = caminho.Split(dirSeparador);
+
+Console.WriteLine("Os diretórios e arquivo no caminho são: ");
+foreach (var dir in diretorios)
+{
+    Console.WriteLine(dir);
+}
+
+Console.ReadKey();
