9ea632c9754fe9fce22bc653a8fd5166da7d3912
Samuel Vinicius <samuelviniciusoficial@gmail.com>
2025-03-16 18:47:02 -0300

    ValueTask

---
+++ 02TaskProgAssíncrona/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("### Café da manhã Síncrono ###");
+
+
+Console.ReadKey();
+
+static void CafeDaManha()
+{
+    Console.WriteLine("\n-- Preparar o café");
+    var cafe = PrepararCafe();
+
+    Console.WriteLine("\n** Preparar o pão");
+    var pao = PrepararPao();
+
+    ServirCafe(cafe, pao);
+}
