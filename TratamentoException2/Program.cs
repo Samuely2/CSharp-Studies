badc54bc79a133b3826dffbf04ec533aef0ea657
Samuel Vinicius <samuelviniciusoficial@gmail.com>
2025-03-23 20:11:25 -0300

    excecoes com metodos assincronos

---
+++ TratamentoException2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+await LancaExceptionAsync();
+Console.ReadKey();
+
+static async Task LancaExceptionAsync()
+{
+    try
+    {
+        var primeiraTask = Task.Run(() => {
+            Task.Delay(1000);
+            throw new IndexOutOfRangeException
+            ("IndexOutOfRangeException lançada explicitamente.");
+        });
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Bloco catch executado");
+        Console.WriteLine(ex.Message);
+    }
+}
+//Quando se tem uma exceção dentro de um método assincrono, tem que usar o try catch dentro do método para poder tratar
---
+++ TratamentoExcpetion/Program.cs
@@ -0,0 +1,30 @@
+﻿TesteAsync t = new();
+
+
+
+Console.ReadKey();
+
+
+class TesteAsync
+{
+    public Task MinhaTarefaAsync()
+    {
+        return Task.Run(() => {
+            Task.Delay(2000);
+            throw new Exception("Minha exception...");
+        });
+    }
+    public async void ChamaTarefaAsync() //tem que dar o try catch dentro do método assincrono para tratar a exceção
+    {
+        try
+        {
+            await MinhaTarefaAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Bloco catch foi executado");
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+
