f69841cc52b2a850ebc9562039ec144cec0f41fc
Samuel Vinicius <samuelviniciusoficial@gmail.com>
2025-03-23 23:40:22 -0300

    streams assincronos

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+        
+            //var meses = new List<string>() { "Janeiro", "Fevereiro", "Março", "Abril" };
+            await foreach (var mes in GeraMeses())
+            {
+                Console.WriteLine(mes);
+            }
+            Console.ReadLine();
+        }
+        static private async IAsyncEnumerable<string> GeraMeses()
+        {
+            yield return "janeiro";
+            yield return "fevereiro";
+            await Task.Delay(2000);
+            yield return "março";
+            yield return "abril";
+        }
+    }
+}
---
+++ StreamAsync/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Console.ReadKey();
+
+
+//var meses = new List<string>() { "Janeiro", "Fevereiro", "Março", "Abril" };
+foreach (var mes in GeraMeses)
+{
+    Console.WriteLine(mes);
+}
+Console.ReadLine();
+
+static private IEnumerable<string> GeraMeses()
+{
+    yield return "janeiro";
+    yield return "fevereiro";
+    yield return "março";
+    yield return "abril";
+}
