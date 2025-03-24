8499da9eecfba057f22c8e4995a8c4ad1abbbfb9
Samuel Vinicius <samuelviniciusoficial@gmail.com>
2025-03-24 00:08:22 -0300

    Threads com semaforos

---
+++ SemaphoreSlim_/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SemaphoreSlim_
+{
+    internal class Program
+    {
+        private static SemaphoreSlim semaforoSlim = new SemaphoreSlim(4);
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 6; i++)
+            {
+                string threadName = "Thread " + i;
+                int espera = 2 + 2 * i;
+
+                var t = new Thread(() => AcessarBancoDados(threadName, espera));
+
+            t.Start();
+            }
+        }
+        private static void AcessarBancoDados(string nome, int seconds)
+        {
+            Console.WriteLine($"{nome} aguardando acesso ao banco de dados...");
+            semaforoSlim.Wait();
+
+            Console.WriteLine($"{nome} autorizada a acessar ao banco de dados...");
+            Console.WriteLine($"{nome} concluida...");
+            semaforoSlim.Release();
+        }
+    }
+}
---
+++ Semaphore_/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Semaphore_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cria um semáforo que permite no máximo 2 threads
+            var semaphore = new Semaphore(1, 2);
+
+            Console.WriteLine("Inclui a thread no semáforo");
+            // Inclui a thread no semáforo usando WaitOne
+            semaphore.WaitOne();
+
+            // Executa o método
+            Console.WriteLine("Executou o método");
+
+            // Libera a thread do semáforo
+            semaphore.Release();
+
+            Console.WriteLine("Liberou a thread do semáforo");
+            Console.ReadLine();
+
+        }
+    }
+}
