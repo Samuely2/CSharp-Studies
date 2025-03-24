1de6998a4019553ebae9862710fbe370d38688eb
Samuel Vinicius <samuelviniciusoficial@gmail.com>
2025-03-23 22:14:13 -0300

    tratamento multiplas ex's

---
+++ TratamentoMultiplasExceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿
+await LancaMultiplasExcecoesAsync();
+
+Console.ReadKey();
+
+static async Task LancaMultiplasExcecoesAsync()
+{
+	Task? tarefas = null;
+	try
+	{
+		var primeiraTask = Task.Run(() =>
+		{
+			Task.Delay(1000);
+			throw new IndexOutOfRangeException("IndexOutOfRangeException lançada explicitamente");
+		});
+		var segundaTask = Task.Run(() =>
+		{
+			Task.Delay(1000);
+			throw new InvalidOperationException("InvalidOperationException lançada explicitamente");
+		});
+		tarefas = Task.WhenAll(primeiraTask, segundaTask);
+		await tarefas;
+	}
+	catch
+	{
+		Console.WriteLine("Ocorreram as seguintes exceções:");
+		AggregateException TodasExceptions = tarefas.Exception; // Obtém todas as exceções internas, e armazena em TodasException
+		// que é uma instância de AggregateException
+
+		foreach (var ex in TodasExceptions.InnerExceptions)
+		{
+			Console.WriteLine(ex.GetType().ToString);
+		}
+	}
+}
